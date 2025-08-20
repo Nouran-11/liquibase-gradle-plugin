a373da7b511d19d72c2d2c57fee66ed8e4336923
Rafael Chaves <rchaves@gradle.com>
2025-08-20 14:32:22 -0300

    Fix ArgumentBuilderTest to be compatible with `ProjectInfo`

--- src/main/groovy/org/liquibase/gradle/ProjectInfo.groovy
+++ src/main/groovy/org/liquibase/gradle/ProjectInfo.groovy
@@ -8,36 +8,41 @@ class ProjectInfo {
     String runList
     List<String> jvmArgs
     Map<String, Object> liquibaseProperties
-    String buildDirPath
+    File buildDir
     Object logger
 
-    ProjectInfo(List<Activity> activities, String runList, List<String> jvmArgs, Map<String, Object> liquibaseProperties, String buildDirPath, Object logger) {
+    ProjectInfo(List<Activity> activities, String runList, List<String> jvmArgs, Map<String, Object> liquibaseProperties, File buildDir, Object logger) {
         this.activities = activities
         this.runList = runList
         this.jvmArgs = jvmArgs
         this.liquibaseProperties = liquibaseProperties
-        this.buildDirPath = buildDirPath
+        this.buildDir = buildDir
         this.logger = logger
     }
 
     static ProjectInfo fromProject(Project project) {
-        def activities = project.liquibase.activities.toList()
-        def runList = project.liquibase.runList
-        def jvmArgs = project.liquibase.jvmArgs
-        def buildDirPath = project.buildDir.absolutePath
+        def buildDir = project.buildDir
         def logger = project.logger
         def liquibaseProperties = [:]
         project.properties.findAll { key, value ->
             if (!key.startsWith("liquibase")) return false
             if (value != null && LiquibaseTask.class.isAssignableFrom(value.class)) return false
-            return ArgumentBuilder.allGlobalProperties.contains(key) ||
+
+            //XXX: @Nouran Atef - this ends up ignoring `liquibaseChangelogParameters`
+            def supported = ArgumentBuilder.allGlobalProperties.contains(key) ||
                     ArgumentBuilder.allCommandProperties.contains(key)
+            if (!supported) {
+                println("Skipping unsupported: $key")
+            }
+            return supported
         }.each { key, value ->
             liquibaseProperties[key] = value
         }
-
+        def activities = project.liquibase.activities.toList()
+        def runList = project.liquibase.runList
+        def jvmArgs = project.liquibase.jvmArgs
         return new ProjectInfo(activities, runList, jvmArgs,
-                liquibaseProperties as Map<String, Object>, buildDirPath, logger)
+                liquibaseProperties as Map<String, Object>, buildDir, logger)
     }
 
     @Internal
