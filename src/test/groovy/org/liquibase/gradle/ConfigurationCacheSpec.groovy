474a1996ef19a0a40c229a77e62aeea9e54bea13
nouranatef <engineer_nouran@yahoo.com>
2025-08-20 17:28:34 +0300

    cc test update to junit

--- src/test/groovy/org/liquibase/gradle/ConfigurationCacheSpec.groovy
+++ src/test/groovy/org/liquibase/gradle/ConfigurationCacheSpec.groovy
@@ -1,74 +1,76 @@
 package org.liquibase.gradle
 
 import org.gradle.testkit.runner.GradleRunner
-import spock.lang.Specification
-import spock.lang.TempDir
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
 
-class ConfigurationCacheSpec extends Specification {
-    @TempDir
-    File testProjectDir
+import static org.junit.Assert.assertTrue
 
-    def "plugin should be compatible with configuration cache"() {
-        given:
-        def changelogFile = new File(testProjectDir, "changelog.yml")
-        def buildFile = new File(testProjectDir, "build.gradle")
+class ConfigurationCacheSpec {
+    @Rule
+    public TemporaryFolder testProjectDir = new TemporaryFolder()
+
+    @Test
+    void pluginShouldBeCompatibleWithConfigurationCache() {
+        def projectDir = testProjectDir.root
+        def changelogFile = new File(projectDir, "changelog.yml")
+        def buildFile = new File(projectDir, "build.gradle")
         buildFile.text = """
-            plugins {
-                id 'org.liquibase.gradle'
-            }
+           plugins {
+               id 'org.liquibase.gradle'
+           }
 
-            repositories {
-                mavenCentral()
-            }
+           repositories {
+               mavenCentral()
+           }
 
-            dependencies {
-                liquibaseRuntime 'org.liquibase:liquibase-core:4.26.0'
-                liquibaseRuntime 'info.picocli:picocli:4.7.5'
-                liquibaseRuntime 'com.h2database:h2:2.2.224'
-            }
+           dependencies {
+               liquibaseRuntime 'org.liquibase:liquibase-core:4.26.0'
+               liquibaseRuntime 'info.picocli:picocli:4.7.5'
+               liquibaseRuntime 'com.h2database:h2:2.2.224'
+           }
 
-            liquibase {
-                activities {
-                    main {
-                        changelogFile 'changelog.yml'
-                        url 'jdbc:h2:mem:testdb'
-                        username 'sa'
-                        password '1234'
-                        searchPath '${testProjectDir}'
-                    }
-                }
-                runList = 'main'
-            }
-        """
+           liquibase {
+               activities {
+                   main {
+                       changelogFile 'changelog.yml'
+                       url 'jdbc:h2:mem:testdb'
+                       username 'sa'
+                       password '1234'
+                       searchPath "${projectDir}"
+                   }
+               }
+               runList = 'main'
+           }
+       """
 
         changelogFile.text = """
-            databaseChangeLog:
-              - changeSet:
-                  id: 1
-                  author: your.name
-                  changes:
-                    - createTable:
-                        tableName: person
-                        columns:
-                          - column:
-                              name: id
-                              type: int
-                              autoIncrement: true
-                              constraints:
-                                primaryKey: true
-                                nullable: false
-        """
+           databaseChangeLog:
+             - changeSet:
+                 id: 1
+                 author: your.name
+                 changes:
+                   - createTable:
+                       tableName: person
+                       columns:
+                         - column:
+                             name: id
+                             type: int
+                             autoIncrement: true
+                             constraints:
+                               primaryKey: true
+                               nullable: false
+       """
 
-        when:
         def result = GradleRunner.create()
-                .withProjectDir(testProjectDir)
+                .withProjectDir(projectDir)
                 .withPluginClasspath()
                 .withArguments("update", "--configuration-cache","-s")
                 .forwardOutput()
                 .build()
 
-        then:
-        result.output.contains("BUILD SUCCESSFUL")
-        result.output.contains("Configuration cache entry stored.")
+        assertTrue(result.output.contains("BUILD SUCCESSFUL"))
+        assertTrue(result.output.contains("Configuration cache entry stored."))
     }
 }
